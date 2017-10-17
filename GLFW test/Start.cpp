fbb99abed756bbe14a0e8c1f4842aad495d13a65
criscross01 <criscross03@gmail.com>
2017-10-16 21:32:22 -0500

    Worked on GLFW test some more

--- GLFW test/Start.cpp
+++ GLFW test/Start.cpp
@@ -3,13 +3,81 @@
 #include <iostream>
 #include <string>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main()
 {
 	//Initializes glfw variable and assign return value to check if initialization was a success
 	bool initErr = glfwInit();
 
 	//Checks to see if initialization worked
-	if (! initErr) { return 1; }
+	if (!initErr) { return -1; }
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Makes window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW test", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//Makes the current context of the thread the window
+	glfwMakeContextCurrent(window);
+
+	//True if GLAD not initialized
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Sets up the viewport
+	glViewport(0, 0, 800, 600);
+
+	//Sets up to where everytime the window is resized, it calls the framebuffer_size_callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+
+	//Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Gets input
+		processInput(window);
+
+		//Rendering
+		glClearColor(0.5f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Check and call events and swap buffers
+		glfwPollEvents();
+		glfwSwapBuffers(window);		
+	}
+
+
+
+	glfwTerminate();
 
 	return 0;
+}
+
+//Called when window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//Processes input
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
