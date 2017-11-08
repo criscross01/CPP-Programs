01f8d2df67cefe55c5448d3cf5433fb6acd14e8b
criscross01 <criscross03@gmail.com>
2017-11-07 23:12:47 -0600

    :)

--- GLFW test/Start.cpp
+++ GLFW test/Start.cpp
@@ -13,9 +13,15 @@ int width{ 800 }, height{ 600 };
 const char* title{ "What I saw dude" };
 
 float vertices[] = {
-	-0.5f,-0.5f,0.0f,
-	 0.5f,-0.5f,0.0f,
-	 0.0f, 0.5f,0.0f
+	-0.5f,-0.5f,0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f, //Bottom Left
+	 0.5f,-0.5f,0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f, //Bottom Right
+	 0.5f, 0.5f,0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, //Top Right
+	 -0.5f,0.5f,0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f  //Top Left
+};
+
+int index[] = {
+	0,1,2,
+	0,3,2
 };
 
 void glFramebufferSizeCallback(GLFWwindow* window, int width, int height);
@@ -57,18 +63,42 @@ int main()
 
 	unsigned char *data = stbi_load("Container.jpg", &width, &height, &nrChannels, 0);
 
-	unsigned int VBO, VAO; 
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
+
+
+	unsigned int VBO, VAO, EBO; 
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0);
 
@@ -80,7 +110,9 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		shader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
